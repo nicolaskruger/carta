ed9d054f485af7cc60a4187bed8510fd14aca20e
ni <nicolas.nkruger@gmail.com>
2026-06-01 18:57:45 -0300

    save

--- src/value_objects/club_invite.rs
+++ src/value_objects/club_invite.rs
@@ -5,6 +5,12 @@ pub struct ClubInvite {
 }
 
 impl ClubInvite {
+    pub fn build() -> Self {
+        Self {
+            _id: Uuid::new_v4(),
+        }
+    }
+
     pub fn new(id: Uuid) -> Self {
         Self { _id: id }
     }
@@ -41,5 +47,14 @@ mod tests {
         let club_invite = club_invite.set_id(id);
 
         assert_eq!(id.to_string(), club_invite.id().to_string());
+    }
+
+    #[test]
+    fn build() {
+        let id = Uuid::new_v4();
+
+        let club_invite = ClubInvite::build();
+
+        assert_ne!(id.to_string(), club_invite.id().to_string());
     }
 }
