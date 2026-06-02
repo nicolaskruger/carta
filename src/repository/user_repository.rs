26d013cfbc6f2c155bd73e68db8a3ab81e1d24e9
ni <nicolas.nkruger@gmail.com>
2026-06-02 06:00:53 -0300

    save

--- src/repository/user_repository.rs
+++ src/repository/user_repository.rs
@@ -1,7 +1,10 @@
+use async_trait::async_trait;
 use uuid::Uuid;
 
 use crate::entity::user::User;
 
+#[cfg_attr(test, mockall::automock)]
+#[async_trait]
 pub trait IUserRepository {
     async fn create(&self, user: User) -> User;
     async fn find(&self, id: Uuid) -> User;
@@ -9,6 +12,7 @@ pub trait IUserRepository {
 
 pub struct UserRepository {}
 
+#[async_trait]
 impl IUserRepository for UserRepository {
     async fn create(&self, user: User) -> User {
         todo!()
--- src/use_case/user/create_user_case.rs
+++ src/use_case/user/create_user_case.rs
@@ -1,22 +1,71 @@
+use uuid::Uuid;
+
 use crate::{entity::user::User, repository::user_repository::IUserRepository};
 
-pub struct CreateUserCase {}
+pub struct CreateUserCase<UR: IUserRepository> {
+    user_repository: UR,
+}
+
+pub struct CreateUser {
+    pub name: String,
+    pub master: Option<Uuid>,
+}
 
 pub struct CreateUserInput {
-    user: User,
-    user_repository: Box<dyn IUserRepository>,
+    user: CreateUser,
 }
 
 pub struct CreateUserOutput {}
 
-impl CreateUserCase {
-    async fn exec(input: CreateUserInput) {
-        todo!()
+impl<UR: IUserRepository> CreateUserCase<UR> {
+    pub fn new(user_repository: UR) -> Self {
+        Self { user_repository }
+    }
+
+    pub async fn exec(&self, input: CreateUserInput) {
+        let create_user = input.user;
+
+        let user = User::new(None, create_user.name, Uuid::new_v4());
+
+        self.user_repository.create(user).await;
     }
 }
 
 #[cfg(test)]
 mod test {
+
+    use super::*;
+
+    use crate::{
+        repository::user_repository::MockIUserRepository,
+        use_case::user::create_user_case::{self, CreateUserCase},
+    };
+
     #[test]
-    fn create_user_root() {}
+    fn new() {
+        let mock = MockIUserRepository::new();
+
+        let _ = CreateUserCase::new(mock);
+    }
+    #[tokio::test]
+    async fn create_user_root() {
+        let mut user_repository = MockIUserRepository::new();
+
+        user_repository
+            .expect_create()
+            .once()
+            .withf(|u| u.name == "anakin" && u.master.is_none() && u.id != Uuid::new_v4())
+            .returning(|u| u);
+
+        let create_user = CreateUser {
+            name: "anakin".into(),
+            master: None,
+        };
+
+        let create_user_case = CreateUserCase::new(user_repository);
+
+        create_user_case
+            .exec(CreateUserInput { user: create_user })
+            .await;
+    }
 }
