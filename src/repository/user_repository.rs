74738eab79b7e9b646ede88cabd0a815ba71843b
ni <nicolas.nkruger@gmail.com>
2026-06-15 06:29:40 -0300

    save

--- src/repository/user_repository.rs
+++ src/repository/user_repository.rs
@@ -1,4 +1,5 @@
 use async_trait::async_trait;
+use sqlx::{Pool, Postgres};
 use uuid::Uuid;
 
 use crate::entity::user::User;
@@ -15,16 +16,59 @@ pub trait IUserRepository {
     async fn find(&self, id: Uuid) -> Result<User, UserRepositoryError>;
 }
 
-pub struct UserRepository {}
+pub struct UserTable {
+    pub id: Uuid,
+    pub name: String,
+    pub password: String,
+    pub master_id: Option<Uuid>,
+}
+
+pub struct PostgressUserRepository {
+    pool: Pool<Postgres>,
+}
+
+impl PostgressUserRepository {
+    pub fn new(pool: Pool<Postgres>) -> Self {
+        Self { pool }
+    }
+
+    async fn _find(&self, id: Option<Uuid>) -> Option<User> {
+        if let Some(id) = id {
+            let user_row = sqlx::query_as!(UserTable, "select * from users where id = $1", id)
+                .fetch_one(&self.pool)
+                .await
+                .ok()?;
+
+            Some(User::new(
+                None,
+                user_row.name,
+                user_row.password,
+                user_row.id,
+            ))
+        } else {
+            None
+        }
+    }
+}
 
 #[async_trait]
-impl IUserRepository for UserRepository {
+impl IUserRepository for PostgressUserRepository {
     async fn create(&self, user: User) -> Result<User, UserRepositoryError> {
         todo!()
     }
 
     async fn find(&self, id: Uuid) -> Result<User, UserRepositoryError> {
-        todo!()
+        let user_row = sqlx::query_as!(UserTable, "select * from users where id = $1", id)
+            .fetch_one(&self.pool)
+            .await
+            .map_err(|_| UserRepositoryError::UserNotFound)?;
+
+        Ok(User::new(
+            self._find(user_row.master_id).await,
+            user_row.name,
+            user_row.password,
+            user_row.id,
+        ))
     }
 }
 
