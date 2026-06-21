023f80914a33e51aedd13c9d30d3ff44003bfb59
ni <nicolas.nkruger@gmail.com>
2026-06-21 18:50:42 -0300

    save

--- src/use_case/user/create_root_user_case.rs
+++ src/use_case/user/create_root_user_case.rs
@@ -2,10 +2,7 @@ use uuid::Uuid;
 
 use crate::{
     entity::user::User,
-    repository::{
-        auth_repository::{AuthRepositoryErr, IAuthRepository},
-        user_repository::IUserRepository,
-    },
+    repository::{auth_repository::IAuthRepository, user_repository::IUserRepository},
 };
 
 pub struct CreateRootUserCase<UR: IUserRepository, UA: IAuthRepository> {
@@ -46,7 +43,24 @@ impl<UR: IUserRepository, UA: IAuthRepository> CreateRootUserCase<UR, UA> {
             .await
             .map_err(|_| CreateRootUserError::ExitsRoot)?;
 
-        if exists_root { todo!() } else { Ok(()) }
+        if exists_root {
+            let password = self
+                .auth_repository
+                .hash(&input.user.password)
+                .await
+                .map_err(|_| CreateRootUserError::EncryptPassword)?;
+
+            let root_user = User::new(None, input.user.name, password, Uuid::new_v4());
+
+            self.user_repository
+                .create(root_user)
+                .await
+                .map_err(|_| CreateRootUserError::CreateUser)?;
+
+            Ok(())
+        } else {
+            Ok(())
+        }
     }
 }
 
@@ -109,6 +123,108 @@ mod test {
             .returning(|| Ok(false));
 
         user_repository.expect_create().never();
+
+        let create_root_usercase = CreateRootUserCase::new(user_repository, auth_repository);
+
+        let input = CreateRootUserInput {
+            user: CreateRootUser {
+                name: "my name".into(),
+                password: "my password".into(),
+            },
+        };
+
+        let res = create_root_usercase.exec(input).await;
+
+        assert!(res.is_ok());
+    }
+
+    #[tokio::test]
+    async fn should_not_create_an_root_user_when_created_err() {
+        let mut user_repository = MockIUserRepository::new();
+        let mut auth_repository = MockIAuthRepository::new();
+
+        user_repository
+            .expect_exists_root()
+            .once()
+            .returning(|| Ok(true));
+
+        auth_repository
+            .expect_hash()
+            .once()
+            .returning(|_| Ok("encrypt".to_string()));
+
+        user_repository
+            .expect_create()
+            .once()
+            .returning(|_| Err(UserRepositoryError::UserNotCreated));
+
+        let create_root_usercase = CreateRootUserCase::new(user_repository, auth_repository);
+
+        let input = CreateRootUserInput {
+            user: CreateRootUser {
+                name: "my name".into(),
+                password: "my password".into(),
+            },
+        };
+
+        let res = create_root_usercase.exec(input).await;
+
+        assert!(res.is_err());
+
+        if let Err(res) = res {
+            assert_eq!(res, CreateRootUserError::CreateUser)
+        }
+    }
+
+    #[tokio::test]
+    async fn should_not_create_on_encrypt_password_err() {
+        let mut user_repository = MockIUserRepository::new();
+        let mut auth_repository = MockIAuthRepository::new();
+
+        user_repository
+            .expect_exists_root()
+            .once()
+            .returning(|| Ok(true));
+
+        auth_repository
+            .expect_hash()
+            .once()
+            .returning(|_| Err(AuthRepositoryErr::Hash));
+
+        let create_root_usercase = CreateRootUserCase::new(user_repository, auth_repository);
+
+        let input = CreateRootUserInput {
+            user: CreateRootUser {
+                name: "my name".into(),
+                password: "my password".into(),
+            },
+        };
+
+        let res = create_root_usercase.exec(input).await;
+
+        assert!(res.is_err());
+
+        if let Err(res) = res {
+            assert_eq!(res, CreateRootUserError::EncryptPassword)
+        }
+    }
+
+    #[tokio::test]
+    async fn should_create() {
+        let mut user_repository = MockIUserRepository::new();
+        let mut auth_repository = MockIAuthRepository::new();
+
+        user_repository
+            .expect_exists_root()
+            .once()
+            .returning(|| Ok(true));
+
+        auth_repository
+            .expect_hash()
+            .once()
+            .returning(|_| Ok("encrypt".to_string()));
+
+        user_repository.expect_create().once().returning(Ok);
 
         let create_root_usercase = CreateRootUserCase::new(user_repository, auth_repository);
 
